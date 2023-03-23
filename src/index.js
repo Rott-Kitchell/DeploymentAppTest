0638d4316e8ca3c5d4bb0e3ba872c8628cca50f4
Rott Kitchell <r.kitchell815@gmail.com>
2023-03-23 13:28:31 -0500

    added comments, env sample

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//import "marko/node-require";
 require("@marko/compiler/register");
 const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
@@ -10,7 +9,6 @@ import indexPage from "./routes/pages/index.cjs";
 
 import hookRouter from "./routes/hooks/hooks.router.js";
 
-
 const app = express();
 
 app.use(express.json());
@@ -18,37 +16,39 @@ app.use("/assets", express.static("dist/assets"));
 app.use(compressionMiddleware());
 
 app.use(markoMiddleware());
-
+//this hook path is currently the only one working
 app.use("/hook", hookRouter);
 app.use("/", indexPage);
-const port = parseInt(process.env.MYPORT)
-console.log(port)
+const port = parseInt(process.env.MYPORT);
+console.log(port);
 app.listen(port, listener);
 
-// this is for testing purposes only, will need to rewrite before publishing
-const fetch = require('node-fetch')
+// _________________________________________________________________________//
+// the following is for testing purposes only, will need to rewrite before publishing
+// _________________________________________________________________________//
+const fetch = require("node-fetch");
 const ngrok = require("ngrok");
-let ngrokURL = ""
-let hookId
+let ngrokURL = "";
+let hookId;
 const nodemon = require("nodemon");
 
-const BCSTOREHASH = process.env.BCSTOREHASH
-const BCACCESSTOKEN = process.env.BCACCESSTOKEN
-const BCCLIENTID = process.env.BCCLIENTID
-const NGROKTOKEN = process.env.NGROKTOKEN
+const BCSTOREHASH = process.env.BCSTOREHASH;
+const BCACCESSTOKEN = process.env.BCACCESSTOKEN;
+const BCCLIENTID = process.env.BCCLIENTID;
+const NGROKTOKEN = process.env.NGROKTOKEN;
 const ngrokBody = {
   proto: "http",
   addr: port,
-  authtoken: NGROKTOKEN
-}
-console.log(BCSTOREHASH, BCACCESSTOKEN, BCCLIENTID, NGROKTOKEN, ngrokBody)
-const headers = { 
+  authtoken: NGROKTOKEN,
+};
+console.log(BCSTOREHASH, BCACCESSTOKEN, BCCLIENTID, NGROKTOKEN, ngrokBody);
+const headers = {
   "Content-Type": "application/json",
   "X-Auth-Token": BCACCESSTOKEN,
-  "Accept": "application/json"
-}
+  Accept: "application/json",
+};
 
-
+// does the fetching
 async function fetchJson(url, options, onCancel) {
   try {
     const response = await fetch(url, options);
@@ -66,64 +66,85 @@ async function fetchJson(url, options, onCancel) {
     return Promise.resolve(onCancel);
   }
 }
-
+// whenever you start the app, it will update the BigCommerce hook with the new tunnel address (this will be deleted eventually, only for testing purposes)
 export async function updateHook(hookId, data) {
-  const url = new URL(`https://api.bigcommerce.com/stores/${BCSTOREHASH}/v3/hooks/${hookId}`);
+  const url = new URL(
+    `https://api.bigcommerce.com/stores/${BCSTOREHASH}/v3/hooks/${hookId}`
+  );
   const options = {
     method: "PUT",
     headers,
     body: JSON.stringify({ ...data }),
   };
-  console.log(options)
+  console.log(options);
   return await fetchJson(url, options, {});
 }
 
-
+// finds the current hooks associated to the BigCommerce store
 export async function listHooks(params, signal) {
-  const url = new URL(`https://api.bigcommerce.com/stores/${BCSTOREHASH}/v3/hooks`);
-  if (params) Object.entries(params).forEach(([key, value]) => {
-    return url.searchParams.append(key, value.toString());
-  });
-  return await fetchJson(url, { headers, signal }, {})
-    
+  const url = new URL(
+    `https://api.bigcommerce.com/stores/${BCSTOREHASH}/v3/hooks`
+  );
+  if (params)
+    Object.entries(params).forEach(([key, value]) => {
+      return url.searchParams.append(key, value.toString());
+    });
+  return await fetchJson(url, { headers, signal }, {});
 }
-
+// finds the id of the hook
 export function findHookID(data, scope, clientId) {
-  return data.find(hook => hook.scope === scope && hook.client_id === clientId).id
+  return data.find(
+    (hook) => hook.scope === scope && hook.client_id === clientId
+  ).id;
 }
 
+/* 
+Ok, here's what's happening:
+1. ngrok tunnel is started
+2. url of tunnel is stored as ngrokURL
+2. listHooks is called then findHookID is called to find the id of the hook we need to update
+3. updateHook is called to update the hook
+4. nodemon is called while (in theory) excluding the ngrok tunnel since it doesn't need to be restarted. (WIP)
+*/
 ngrok
-  .connect(ngrokBody).then((url) => {
-    ngrokURL = url
+  .connect(ngrokBody)
+  .then((url) => {
+    ngrokURL = url;
     console.log(`ngrok tunnel opened at: ${ngrokURL}`);
     console.log("Open the ngrok dashboard at: https://localhost:4040\n");
-    listHooks().then(data=>{
-    return hookId = findHookID(data, 'store/order/*', BCCLIENTID)
-    }).then(hookId => {
-      let body = {
-        "scope": "store/order/*",
-        "destination": `${ngrokURL}/hook`,
-        "is_active": true,
-        "events_history_enabled": true
-        
-      }
-      
-      updateHook(hookId, body)
-    }).then(()=>{
-      nodemon({
-        exec: `NGROK_URL=${url} node`,
-      }).on("start", () => {
-        console.log("The application has started");
-      }).on("restart", files => {
-        console.group("Application restarted due to:")
-        files.forEach(file => console.log(file));
-        console.groupEnd();
-      }).on("quit", () => {
-        console.log("The application has quit, closing ngrok tunnel");
-        ngrok.kill().then(() => process.exit(0));
+    listHooks()
+      .then((data) => {
+        return (hookId = findHookID(data, "store/order/*", BCCLIENTID));
+      })
+      .then((hookId) => {
+        let body = {
+          scope: "store/order/*",
+          destination: `${ngrokURL}/hook`,
+          is_active: true,
+          events_history_enabled: true,
+        };
+
+        updateHook(hookId, body);
+      })
+      .then(() => {
+        nodemon({
+          exec: `NGROK_URL=${url} node`,
+        })
+          .on("start", () => {
+            console.log("The application has started");
+          })
+          .on("restart", (files) => {
+            console.group("Application restarted due to:");
+            files.forEach((file) => console.log(file));
+            console.groupEnd();
+          })
+          .on("quit", () => {
+            console.log("The application has quit, closing ngrok tunnel");
+            ngrok.kill().then(() => process.exit(0));
+          });
       });
-    })
-    }).catch((error) => {
+  })
+  .catch((error) => {
     console.error("Error opening ngrok tunnel: ", error);
     process.exitCode = 1;
   });
